758ea5168d20d1875eb0218fd4d6117d6e620761
Lu Yiting <e0774189@u.nus.edu>
2022-10-10 19:23:12 +0800

    Fix the issue of StudentAddCommand not accepting a TutorialGroup parameter

--- src/main/java/seedu/address/logic/parser/StudentAddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/StudentAddCommandParser.java
@@ -30,7 +30,8 @@ public class StudentAddCommandParser implements Parser<StudentAddCommand> {
      */
     public StudentAddCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_TAG);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE,
+                        PREFIX_EMAIL, PREFIX_TAG, PREFIX_TUTORIAL_GROUP);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL)
                 || !argMultimap.getPreamble().isEmpty()) {
