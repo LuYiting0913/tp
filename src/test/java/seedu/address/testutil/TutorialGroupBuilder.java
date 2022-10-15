b093c74ac06d2ca4cdd450449366c8b34d71f12a
Lu Yiting <e0774189@u.nus.edu>
2022-10-15 21:07:08 +0800

    Create tutorial group builder and TutorialAddCommandTest

---
+++ src/test/java/seedu/address/testutil/TutorialGroupBuilder.java
@@ -0,0 +1,46 @@
+package seedu.address.testutil;
+
+import seedu.address.model.student.*;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.util.SampleDataUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A utility class to help with building Student objects.
+ */
+public class TutorialGroupBuilder {
+
+    public static final String DEFAULT_TUTORIAL_GROUP = "T01";
+
+    private TutorialGroup tutorialGroup;
+
+
+    /**
+     * Creates a {@code StudentBuilder} with the default details.
+     */
+    public TutorialGroupBuilder() {
+        tutorialGroup = new TutorialGroup(DEFAULT_TUTORIAL_GROUP);
+    }
+
+    /**
+     * Initializes the StudentBuilder with the data of {@code StudentToCopy}.
+     */
+    public TutorialGroupBuilder(TutorialGroup tutorialGroupToCopy) {
+        tutorialGroup = tutorialGroupToCopy;
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code Student} that we are building.
+     */
+    public TutorialGroupBuilder withName(String name) {
+        tutorialGroup = new TutorialGroup(name);
+        return this;
+    }
+
+    public TutorialGroup build() {
+        return this.tutorialGroup;
+    }
+
+}
