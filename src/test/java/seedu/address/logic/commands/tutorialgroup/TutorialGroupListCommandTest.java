b7c1dbbb40c17fc1761e49ac1abb5ff3e2886877
Lu Yiting <e0774189@u.nus.edu>
2022-10-19 15:53:45 +0800

    Fix styling

--- src/test/java/seedu/address/logic/commands/tutorialgroup/TutorialGroupListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/tutorialgroup/TutorialGroupListCommandTest.java
@@ -8,7 +8,6 @@ import static seedu.address.testutil.TypicalStudents.getTypicalAddressBook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 import seedu.address.logic.commands.CommandTestUtil;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
