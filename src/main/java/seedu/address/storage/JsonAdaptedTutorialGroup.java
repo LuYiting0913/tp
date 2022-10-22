b61bf668b8ede3a32f5f1adeae7eb046f37c9ead
Lu Yiting <e0774189@u.nus.edu>
2022-10-22 16:29:23 +0800

    Resolve test failures

--- src/main/java/seedu/address/storage/JsonAdaptedTutorialGroup.java
+++ src/main/java/seedu/address/storage/JsonAdaptedTutorialGroup.java
@@ -21,7 +21,7 @@ class JsonAdaptedTutorialGroup {
      * Constructs a {@code JsonAdaptedPerson} with the given person details.
      */
     @JsonCreator
-    public JsonAdaptedTutorialGroup(@JsonProperty("tutorial") String name) {
+    public JsonAdaptedTutorialGroup(@JsonProperty("tutorialName") String name) {
         this.name = name;
     }
 
